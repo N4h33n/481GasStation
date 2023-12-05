61465f2df8d1adca2d5521ea452e8136f18ab69e
isaacdtc <isaacdatoc@gmail.com>
2023-12-05 13:12:58 -0700

    Fixed the pages from being scrollable n other styling changes lol

--- src/FuelManagment_Page.js
+++ src/FuelManagment_Page.js
@@ -37,122 +37,129 @@ function FuelManagment_Page() {
   return (
     <body style={{height:"100%", width:"100%", backgroundImage: "url('/dashboard_bg2.jpg')", backgroundSize:"cover", position:"absolute" }}>
       <Sidebars />
-      <div className="Fuel_Management">Fuel Managment</div>
-      <div className="Best_Sellers">Sales Overview</div>
 
-      <div className='Fuel_Management_Buttons'>
-		<Link to="/Fuel_BarGraphs" className='dashboard-button' style={{border:'2px solid black'}}>
-			<button className='dashboard-button'>View Fuel</button>
-		</Link> 
-		<Link to="/Sales_Trends_Fuel" className='dashboard-button' style={{border:'2px solid black'}}>
-			<button className='dashboard-button'>Sales Trends</button>
-		</Link>
-		<Link to="/OrderFuel" className='dashboard-button' style={{border:'2px solid black'}}>
-			<button className='dashboard-button'>Order Fuel</button>
-		</Link>
-      </div>
+	 	<div className="Fuel_Management">Fuel Management</div>
+
+		<div className="InventoryManagementBody">
+
+			<div className='Inventory_Management_Buttons'>
+				<Link to="/Fuel_BarGraphs" className='dashboard-button' style={{border:'2px solid black'}}>
+					<button className='dashboard-button'>View Fuel</button>
+				</Link> 
+				<Link to="/Sales_Trends_Fuel" className='dashboard-button' style={{border:'2px solid black'}}>
+					<button className='dashboard-button'>Sales Trends</button>
+				</Link>
+				<Link to="/OrderFuel" className='dashboard-button' style={{border:'2px solid black'}}>
+					<button className='dashboard-button'>Order Fuel</button>
+				</Link>
+			</div>
+			
+			<div className='Best_Sellers_Div'>
+
+				<div className="Best_Sellers_Label_Div">
+					<div className="Best_Sellers">Sales Overview</div>
+					<select className='trend' onChange={handleChange}>
+						<option className="Day30" id="Day30" value="30">Last Day 30</option>
+						<option className="Day60" id="Day60" value="60">Last Day 60</option>
+						<option className="Day90" id="button90" value="90">Last Day 90</option>
+					</select>
+				</div>
+
+				<div className="Best_Sellers_Table_Div">
+					{fourthState && (
+						<table className="Sales_Table">
+							<thead className="HeaderRow">
+								<th>Fuel</th>
+								<th>Units Sold</th>
+							</thead>
+							
+							<tr>
+								<td>Octane 87</td>
+								<td>10000</td>
+							</tr>
+							<tr>
+								<td>Octane 89</td>
+								<td>3400</td>
+							</tr>
+							<tr>
+								<td>Octane 93</td>
+								<td>23000</td>
+							</tr>
+							<tr>
+								<td>Diesel</td>
+								<td>1000</td>
+							</tr>
+							<tr>
+								<td>Propane</td>
+								<td>400</td>
+							</tr>
+						</table>
+					)}
+
+					{fifthState && (
+					<table className="Sales_Table">
+							<thead className="HeaderRow">
+								<th>Fuel</th>
+								<th>Units Sold</th>
+							</thead>
+							
+							<tr>
+								<td>Octane 87</td>
+								<td>31040</td>
+							</tr>
+							<tr>
+								<td>Octane 89</td>
+								<td>5790</td>
+							</tr>
+							<tr>
+								<td>Octane 93</td>
+								<td>18000</td>
+							</tr>
+							<tr>
+								<td>Diesel</td>
+								<td>2000</td>
+							</tr>
+							<tr>
+								<td>Propane</td>
+								<td>700</td>
+							</tr>
+						</table>
+					)}
+
+
+					{sixthState && (
+					<table className="Sales_Table">
+							<thead className="HeaderRow">
+								<th>Fuel</th>
+								<th>Units Sold</th>
+							</thead>
+							
+							<tr>
+								<td>Octane 87</td>
+								<td>95600</td>
+							</tr>
+							<tr>
+								<td>Octane 89</td>
+								<td>11990</td>
+							</tr>
+							<tr>
+								<td>Octane 93</td>
+								<td>89000</td>
+							</tr>
+							<tr>
+								<td>Diesel</td>
+								<td>3000</td>
+							</tr>
+							<tr>
+								<td>Propane</td>
+								<td>1400</td>
+							</tr>
+						</table>
+					)}
+				</div>
+			</div>
+		</div>
       
-	  <select className='trend' onChange={handleChange}>
-		<option className="Day30" id="Day30" value="30">Last Day 30</option>
-		<option className="Day60" id="Day60" value="60">Last Day 60</option>
-		<option className="Day90" id="button90" value="90">Last Day 90</option>
-	  </select>
-	  
-	<div className='Best_Sellers_Div'>
-
-{fourthState && (
-	<table className="Sales_Table">
-		<thead className="HeaderRow">
-			<th>Fuel</th>
-			<th>Units Sold</th>
-		</thead>
-		
-		<tr>
-			<td>Octane 87</td>
-			<td>10000</td>
-		</tr>
-		<tr>
-			<td>Octane 89</td>
-			<td>3400</td>
-		</tr>
-		<tr>
-			<td>Octane 93</td>
-			<td>23000</td>
-		</tr>
-		<tr>
-			<td>Diesel</td>
-			<td>1000</td>
-		</tr>
-		<tr>
-			<td>Propane</td>
-			<td>400</td>
-		</tr>
-	</table>
-)}
-
-{fifthState && (
-  <table className="Sales_Table">
-		<thead className="HeaderRow">
-			<th>Fuel</th>
-			<th>Units Sold</th>
-		</thead>
-		
-		<tr>
-			<td>Octane 87</td>
-			<td>31040</td>
-		</tr>
-		<tr>
-			<td>Octane 89</td>
-			<td>5790</td>
-		</tr>
-		<tr>
-			<td>Octane 93</td>
-			<td>18000</td>
-		</tr>
-		<tr>
-			<td>Diesel</td>
-			<td>2000</td>
-		</tr>
-		<tr>
-			<td>Propane</td>
-			<td>700</td>
-		</tr>
-	</table>
-)}
-
-
-{sixthState && (
-  <table className="Sales_Table">
-		<thead className="HeaderRow">
-			<th>Fuel</th>
-			<th>Units Sold</th>
-		</thead>
-		
-		<tr>
-			<td>Octane 87</td>
-			<td>95600</td>
-		</tr>
-		<tr>
-			<td>Octane 89</td>
-			<td>11990</td>
-		</tr>
-		<tr>
-			<td>Octane 93</td>
-			<td>89000</td>
-		</tr>
-		<tr>
-			<td>Diesel</td>
-			<td>3000</td>
-		</tr>
-		<tr>
-			<td>Propane</td>
-			<td>1400</td>
-		</tr>
-	</table>
-)}
-
-  
-</div>
     </body>
   );
 }
--- src/InventoryManagment_Page.js
+++ src/InventoryManagment_Page.js
@@ -35,153 +35,163 @@ function App() {
   return (
     <body style={{height:"100%", width:"100%", backgroundImage: "url('/dashboard_bg2.jpg')", backgroundSize:"cover", position:"absolute" }}>
       <Sidebars />
-      <div className="Fuel_Management">Inventory Managment</div>
-      <div className="Best_Sellers">Best Sellers</div>
+	  
 
-      <div className='Inventory_Management_Buttons'>
-		  <Link to="/BarGraph" className='dashboard-button' style={{border:'2px solid black'}}>
-			<button className='dashboard-button'>Inventory</button>
-		  </Link> 
-		  <Link to="/Sales_Trends" className='dashboard-button' style={{border:'2px solid black'}}>
-			<button className='dashboard-button'>Sales Trends</button>
-		  </Link> 
-		  <Link to="/Order_Inventory" className='dashboard-button' style={{border:'2px solid black'}}>
-			<button className='dashboard-button'>Order Inventory</button>
-		  </Link> 
-		  <Link to="/Set_Prices" className='dashboard-button' style={{border:'2px solid black'}}>
-			<button className='dashboard-button'>Set Prices</button>
-		  </Link> 
-      </div>
-	  <select className='trend' onChange={handleChange}>
-		<option className="Day30" id="Day30" value="30">Last Day 30</option>
-		<option className="Day60" id="Day60" value="60">Last Day 60</option>
-		<option className="Day90" id="button90" value="90">Last Day 90</option>
-	  </select>
-	<div className='Best_Sellers_Div'>
+		<div className="Fuel_Management">Inventory Management</div>
 
-{fourthState && (
-	<div class="cards">
-		<div class="sales_card">
-		  <img src='tea.png' alt="Avatar"></img>
-		  <div class="cardcontainer">
-			<h4><b>Tea</b></h4>
-			<p>62</p>
-		  </div>
-	  </div>
-	  <div class="sales_card">
-		  <img src='oreo.png' alt="Avatar"></img>
-		  <div class="cardcontainer">
-			<h4><b>Oreo</b></h4>
-			<p>50</p>
-		  </div>
-	  </div>
-	  <div class="sales_card">
-		  <img src='coffee.png' alt="Avatar"></img>
-		  <div class="cardcontainer">
-			<h4><b>Coffee</b></h4>
-			<p>47</p>
-		  </div>
-	  </div>
-	  <div class="sales_card">
-		  <img src='chips_ahoy.png' alt="Avatar"></img>
-		  <div class="cardcontainer">
-			<h4><b>Chips Ahoy</b></h4>
-			<p>42</p>
-		  </div>
-	  </div>
-	  <div class="sales_card">
-		  <img src='coffee_crisp.png' alt="Avatar"></img>
-		  <div class="cardcontainer">
-			<h4><b>Coffee Crisp</b></h4>
-			<p>36</p>
-		  </div>
-	  </div>
-	</div>
-)}
+		<div className="InventoryManagementBody">
 
-{fifthState && (
-  <div class="cards">
-		<div class="sales_card">
-		  <img src='coffee.png' alt="Avatar"></img>
-		  <div class="cardcontainer">
-			<h4><b>Coffee</b></h4>
-			<p>120</p>
-		  </div>
-	  </div>
-	  <div class="sales_card">
-		  <img src='tea.png' alt="Avatar"></img>
-		  <div class="cardcontainer">
-			<h4><b>Tea</b></h4>
-			<p>111</p>
-		  </div>
-	  </div>
-	  <div class="sales_card">
-		  <img src='chips_ahoy.png' alt="Avatar"></img>
-		  <div class="cardcontainer">
-			<h4><b>Chips Ahoy</b></h4>
-			<p>103</p>
-		  </div>
-	  </div>
-	  <div class="sales_card">
-		  <img src='oreo.png' alt="Avatar"></img>
-		  <div class="cardcontainer">
-			<h4><b>Oreo</b></h4>
-			<p>100</p>
-		  </div>
-	  </div>
-	  <div class="sales_card">
-		  <img src='2l_soda.png' alt="Avatar"></img>
-		  <div class="cardcontainer">
-			<h4><b>2L Soda</b></h4>
-			<p>78</p>
-		  </div>
-	  </div>
-	</div>
-)}
+			<div className='Inventory_Management_Buttons'>
+				<Link to="/BarGraph" className='dashboard-button' style={{border:'2px solid black'}}>
+				<button className='dashboard-button'>Inventory</button>
+				</Link> 
+				<Link to="/Sales_Trends" className='dashboard-button' style={{border:'2px solid black'}}>
+				<button className='dashboard-button'>Sales Trends</button>
+				</Link> 
+				<Link to="/Order_Inventory" className='dashboard-button' style={{border:'2px solid black'}}>
+				<button className='dashboard-button'>Order Inventory</button>
+				</Link> 
+				<Link to="/Set_Prices" className='dashboard-button' style={{border:'2px solid black'}}>
+				<button className='dashboard-button'>Set Prices</button>
+				</Link> 
+			</div>
+
+			<div className='Best_Sellers_Div'>
+		
+				<div className="Best_Sellers_Label_Div">
+					<div className="Best_Sellers">Best Sellers</div>
+
+					<select className='trend' onChange={handleChange}>
+						<option className="Day30" id="Day30" value="30">Last Day 30</option>
+						<option className="Day60" id="Day60" value="60">Last Day 60</option>
+						<option className="Day90" id="button90" value="90">Last Day 90</option>
+					</select>
+				</div>
+
+				<div className='Card_Div'>
+					{fourthState && (
+						<div class="cards">
+							<div class="sales_card">
+							<img src='tea.png' alt="Avatar"></img>
+							<div class="cardcontainer">
+								<h4><b>Tea</b></h4>
+								<p>62</p>
+							</div>
+						</div>
+						<div class="sales_card">
+							<img src='oreo.png' alt="Avatar"></img>
+							<div class="cardcontainer">
+								<h4><b>Oreo</b></h4>
+								<p>50</p>
+							</div>
+						</div>
+						<div class="sales_card">
+							<img src='coffee.png' alt="Avatar"></img>
+							<div class="cardcontainer">
+								<h4><b>Coffee</b></h4>
+								<p>47</p>
+							</div>
+						</div>
+						<div class="sales_card">
+							<img src='chips_ahoy.png' alt="Avatar"></img>
+							<div class="cardcontainer">
+								<h4><b>Chips Ahoy</b></h4>
+								<p>42</p>
+							</div>
+						</div>
+						<div class="sales_card">
+							<img src='coffee_crisp.png' alt="Avatar"></img>
+							<div class="cardcontainer">
+								<h4><b>Coffee Crisp</b></h4>
+								<p>36</p>
+							</div>
+						</div>
+						</div>
+					)}
+
+					{fifthState && (
+					<div class="cards">
+							<div class="sales_card">
+							<img src='coffee.png' alt="Avatar"></img>
+							<div class="cardcontainer">
+								<h4><b>Coffee</b></h4>
+								<p>120</p>
+							</div>
+						</div>
+						<div class="sales_card">
+							<img src='tea.png' alt="Avatar"></img>
+							<div class="cardcontainer">
+								<h4><b>Tea</b></h4>
+								<p>111</p>
+							</div>
+						</div>
+						<div class="sales_card">
+							<img src='chips_ahoy.png' alt="Avatar"></img>
+							<div class="cardcontainer">
+								<h4><b>Chips Ahoy</b></h4>
+								<p>103</p>
+							</div>
+						</div>
+						<div class="sales_card">
+							<img src='oreo.png' alt="Avatar"></img>
+							<div class="cardcontainer">
+								<h4><b>Oreo</b></h4>
+								<p>100</p>
+							</div>
+						</div>
+						<div class="sales_card">
+							<img src='2l_soda.png' alt="Avatar"></img>
+							<div class="cardcontainer">
+								<h4><b>2L Soda</b></h4>
+								<p>78</p>
+							</div>
+						</div>
+						</div>
+					)}
 
 
-{sixthState && (
-  <div class="cards">
-		<div class="sales_card">
-		  <img src='tea.png' alt="Avatar"></img>
-		  <div class="cardcontainer">
-			<h4><b>Tea</b></h4>
-			<p>256</p>
-		  </div>
-	  </div>
-	  <div class="sales_card">
-		  <img src='coffee.png' alt="Avatar"></img>
-		  <div class="cardcontainer">
-			<h4><b>Coffee</b></h4>
-			<p>240</p>
-		  </div>
-	  </div>
-	  <div class="sales_card">
-		  <img src='chips_ahoy.png' alt="Avatar"></img>
-		  <div class="cardcontainer">
-			<h4><b>Chips Ahoy</b></h4>
-			<p>182</p>
-		  </div>
-	  </div>
-	  <div class="sales_card">
-		  <img src='milk.png' alt="Avatar"></img>
-		  <div class="cardcontainer">
-			<h4><b>Milk</b></h4>
-			<p>170</p>
-		  </div>
-	  </div>
-	  <div class="sales_card">
-		  <img src='2l_soda.png' alt="Avatar"></img>
-		  <div class="cardcontainer">
-			<h4><b>2L Soda</b></h4>
-			<p>153</p>
-		  </div>
-	  </div>
-	</div>
-)}
-
-  
-</div>
+					{sixthState && (
+					<div class="cards">
+							<div class="sales_card">
+							<img src='tea.png' alt="Avatar"></img>
+							<div class="cardcontainer">
+								<h4><b>Tea</b></h4>
+								<p>256</p>
+							</div>
+						</div>
+						<div class="sales_card">
+							<img src='coffee.png' alt="Avatar"></img>
+							<div class="cardcontainer">
+								<h4><b>Coffee</b></h4>
+								<p>240</p>
+							</div>
+						</div>
+						<div class="sales_card">
+							<img src='chips_ahoy.png' alt="Avatar"></img>
+							<div class="cardcontainer">
+								<h4><b>Chips Ahoy</b></h4>
+								<p>182</p>
+							</div>
+						</div>
+						<div class="sales_card">
+							<img src='milk.png' alt="Avatar"></img>
+							<div class="cardcontainer">
+								<h4><b>Milk</b></h4>
+								<p>170</p>
+							</div>
+						</div>
+						<div class="sales_card">
+							<img src='2l_soda.png' alt="Avatar"></img>
+							<div class="cardcontainer">
+								<h4><b>2L Soda</b></h4>
+								<p>153</p>
+							</div>
+						</div>
+						</div>
+					)}
+				</div>
+			</div>
+		</div>
     </body>
   );
 }
